8e56cd0a244500429a1bb8b7123cab646d57c248
Francisco Herrera <pakman_198@hotmail.com>
2021-11-01 13:31:24 -0600

    Replaced previous layout with a different onw using CSS

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -3,7 +3,28 @@ import "./index.css";
 export default function Tree() {
   return (
     <div className="tree">
-      <p>root</p>
+      <ul className="root">
+        <li>root</li>
+        <li>
+          <ul>
+            <li>ant</li>
+            <li>
+              <span>bear</span>
+              <ul>
+                <li>cat</li>
+                <li>
+                  <span>dog</span>
+                  <ul>
+                    <li>elephant</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>frog</li>
+          </ul>
+        </li>
+      </ul>
+      {/* <p>root</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
@@ -11,7 +32,7 @@ export default function Tree() {
       <p>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
       </p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p> */}
     </div>
   );
 }
